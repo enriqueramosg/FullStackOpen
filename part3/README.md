90cd99f5c6991553d1bf2da44bbae67cd34bf862
Enrique Ramos <114602227+enriqueramosg@users.noreply.github.com>
2024-10-18 16:02:38 +0100

    Part3b exercises

---
+++ part3/README.md
@@ -0,0 +1,1 @@
+https://fullstackopen-4f18.onrender.com
--- part3/index.js
+++ part3/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors())
 app.use(express.json())
-
+app.use(express.static('dist'))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
@@ -125,8 +128,9 @@ app.post('/api/persons', (request, response) => {
   
     response.json(person)
   })
-  
-const PORT = 3001
+
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
